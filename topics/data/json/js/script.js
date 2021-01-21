5d190565e715b054f59ae54e3a374f6d653cca75
Pippin <pippin.barr@gmail.com>
2021-01-21 10:57:06 -0500

    Finished JSON video

--- topics/data/json/js/script.js
+++ topics/data/json/js/script.js
@@ -1,32 +1,30 @@
 "use strict";
 
-/**
-Title of Project
-Author Name
+let jokeText = ``; // The current joke.
+let jokeData = undefined; // The loaded joke data
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-/**
-Description of preload
-*/
 function preload() {
-
+  jokeData = loadJSON(`https://official-joke-api.appspot.com/jokes/programming/random`);
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  // We get the joke object as the first element of the array
+  let joke = jokeData[0];
+  // Set the joke text as the setup and punchline properties together
+  jokeText = `${joke.setup}\n\n${joke.punchline}`;
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
 
+  // Display the current joke
+  push();
+  fill(255, 255, 0);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+  text(jokeText, width / 2, height / 2, width / 2, height / 2);
+  pop();
 }
