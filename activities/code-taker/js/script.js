1c650f9ca7cf7818ba8c3e843b99dfbb910853eb
Pippin <pippin.barr@gmail.com>
2021-03-10 10:29:13 -0500

    Finished code taker activity

--- activities/code-taker/js/script.js
+++ activities/code-taker/js/script.js
@@ -1,11 +1,37 @@
 /**
-Title of Project
-Author Name
+Code Taker
+Pippin Barr
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Da Vinci?!
 */
 
 "use strict";
 
-// Code goes here
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    // Check if they got it
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
